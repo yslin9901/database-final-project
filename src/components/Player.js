5af5032fe2b215c1d899cfb342c4b8d83da67c43
yslin <s5907610@gmail.com>
2020-07-11 20:27:53 +0800

    feat(Player.js): add iframe to play song on YouTube

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,17 +2,23 @@ import React from 'react';
 import NavbarComponent from './NavbarComponent';
 import Button from 'react-bootstrap/Button';
 import '../App.css';
+//import axios from 'axios';
 
 
 function Player() {
+  var keyword = "deca joins";
+  
+  var base_url = 'http://www.youtube.com/embed?listType=search&list=';
+  var target_url = base_url + keyword;
 
   return (
     <>
       <NavbarComponent />
       <div className="song-player-container">
+        <iframe title="song_player" id="youriframe" width="640" height="360" src={target_url}></iframe>
         <Button variant="primary" className="btn-lg">加入收藏</Button>
-        <img src={require('../fake_song.PNG')} alt="song player" />
       </div>
+      
     </>
   );
 }
