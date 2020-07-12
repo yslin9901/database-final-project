fb6f2c3aa6bb467de6e5134f48ecbcfd44d15e7b
yslin <s5907610@gmail.com>
2020-07-12 23:55:23 +0800

    feat(Collection.js): check user info when accessing Collection

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import NavbarComponent from './NavbarComponent';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios'
 import '../App.css';
 
 function Collection() {
-
+  let history = useHistory()
+  useEffect( () => {
+    const api = '/api/logout'
+    axios
+      .get(api)
+      .then(res => {
+        if (res.data.error) {
+          alert('請先登入')
+          // route to home page
+          history.replace('/')
+        } else {
+          alert('logout successfully')
+        }
+      })
+      .catch(err => console.log(err));
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
   return (
     <>
       <NavbarComponent />
