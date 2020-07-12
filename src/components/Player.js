834b13785ba9abb6bc15afb7ea16cb54ff6d8703
yslin <s5907610@gmail.com>
2020-07-12 20:35:05 +0800

    chore(config.js): store api_key in config

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,45 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useSelector } from 'react-redux'
 import NavbarComponent from './NavbarComponent';
 import Button from 'react-bootstrap/Button';
+import config from '../config';
 import '../App.css';
 
 function Player() {
+
+  const [videoList, setVideoList] = useState([]);
+
   // get playlist from redux
-  const playlist = useSelector(state => state.playlist_info);
-  console.log(playlist);
+  // const playlist = useSelector(state => state.playlist_info);
+  // console.log(playlist);
 
   // fake playlist
-  //const playlist = [{artistname: "Tempalay", songname: "そなちね"}, {artistname: "deca joins", songname: "海浪"}];
-  var video_list = [];
+  const playlist = [{artistname: "Tempalay", songname: "そなちね"}, {artistname: "deca joins", songname: "海浪"}];
   
-  for(let i=0; i<playlist.length; i++){
-    var song=playlist[i].artistname;
-    var artist=playlist[i].songname;
-
-    const api_key = 'AIzaSyAVMV_1I1LLP8TmZSrt-p9UbyOmlO_w1DM';
-    // var temp = [];
-    // get videoId
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${
-      artist+song
-    }&type=video&key=${api_key}`
-    )
-    .then((res) => res.json())
-    .then((data) => {
-      var id = data.items[0]["id"]["videoId"]
-      console.log(id) // video_id
-    }).catch(e => {console.log(e)})
-  }
+  // fake id
+  const fakeVideoId = ['Aa5onMizfco', 'FonjL7DQAUQ']
+  useEffect(() => {
+    for(let i=0; i<playlist.length; i++){
+      var song=playlist[i].artistname;
+      var artist=playlist[i].songname;
+  
+      // var temp = [];
+      // get videoId
+      fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${
+        artist+song
+      }&type=video&key=${config.api_key}`
+      )
+      .then((res) => res.json())
+      .then((data) => {
+        // var id = data.items[0]["id"]["videoId"]
+        // fake id
+        setVideoList([...videoList, fakeVideoId])
+        // console.log(id) // video_id
+      }).catch(e => {console.log(e)})
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+  
+  console.log('video list = ', videoList)
 
   //var target_url = 'http://www.youtube.com/embed?playlist=cbut2K6zvJY,7iw30sK2UCo,sYV5MTy0v1I';
   var target_url = 'http://www.youtube.com/embed?playlist=';
 
   // fake video_ID_list
-  video_list = ["Aa5onMizfco", "FonjL7DQAUQ"];
+  // video_list = ["Aa5onMizfco", "FonjL7DQAUQ"];
 
   //get playlist_url
-  for(let i=0; i<video_list.length; i++){
-    target_url = target_url + video_list[i] + ",";
+  for(let i=0; i<videoList.length; i++){
+    target_url = target_url + videoList[i] + ",";
   }
 
   return (
---
+++ src/config.js
@@ -0,0 +1,4 @@
+const config = () => ({
+  api_key: 'a61a19dca8bc8afa394d511d7e23d4c2',
+});
+export default config();
